584cbcb311ffe7f46b5c69c1b392da57c7104948
Andrés Giraldo <andresgircar@gmail.com>
2024-07-06 09:40:58 -0500

    ajustando server.js

--- api/server.js
+++ api/server.js
@@ -1,12 +1,18 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('/db.json');
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/product/:resource/:id/show': '/resource/:id'
+}))
 server.use(router);
 
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
 console.log(`JSON Server is running on port ${port}`);
 });
+
+module.exports = server;
